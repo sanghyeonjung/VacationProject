32185c56d613775fce9d5656cb33efca33aaf8ff
jeong <roy1109@naver.com>
2022-08-02 14:13:32 +0900

    hello

---
+++ app/src/main/java/com/example/vacationproject/DetailRedFish.kt
@@ -0,0 +1,11 @@
+package com.example.vacationproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailRedFish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_red_fish)
+    }
+}
--- app/src/main/java/com/example/vacationproject/FirstStudyActivity.kt
+++ app/src/main/java/com/example/vacationproject/FirstStudyActivity.kt
@@ -1,11 +1,30 @@
 package com.example.vacationproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class FirstStudyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_study)
+        val redFish = findViewById<ImageButton>(R.id.buttonRedFishStudyActivity)
+        val whiteFish = findViewById<ImageButton>(R.id.buttonWhiteFishStudyActivity)
+        val whiteSlime= findViewById<ImageButton>(R.id.buttonWhiteSlimeStudyActivity)
+        val hair = findViewById<ImageButton>(R.id.buttonHairStudyActivity)
+
+        redFish.setOnClickListener{
+            startActivity(Intent(this@FirstStudyActivity,DetailRedFish::class.java))
+        }
+        whiteFish.setOnClickListener{
+            startActivity(Intent(this@FirstStudyActivity,DetailWhiteFish::class.java))
+        }
+        whiteSlime.setOnClickListener{
+            startActivity(Intent(this@FirstStudyActivity,DetailWhiteSlime::class.java))
+        }
+        hair.setOnClickListener{
+            startActivity(Intent(this@FirstStudyActivity,DetailHair::class.java))
+        }
     }
 }
