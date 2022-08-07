cf06d99d82fb28097ed066eee88da42a8bf1d772
jeong <roy1109@naver.com>
2022-08-07 15:06:33 +0900

    8.3

--- app/src/main/java/com/example/vacationproject/FirstStudyActivity.kt
+++ app/src/main/java/com/example/vacationproject/FirstStudyActivity.kt
@@ -1,9 +1,15 @@
 package com.example.vacationproject
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 
 class FirstStudyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +20,16 @@ class FirstStudyActivity : AppCompatActivity() {
         val whiteSlime= findViewById<ImageButton>(R.id.buttonWhiteSlimeStudyActivity)
         val hair = findViewById<ImageButton>(R.id.buttonHairStudyActivity)
         val back_btn = findViewById<ImageButton>(R.id.backButtonFirstStudyActivity)
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        val mAlertDialog = mBuilder.show()
+        val CancelBtn = mDialogView.findViewById<Button>(R.id.dialogCancelButton)
+        CancelBtn.setOnClickListener{
+            mAlertDialog.dismiss()
+        }
+
 
         redFish.setOnClickListener{
             startActivity(Intent(this@FirstStudyActivity,DetailRedFish::class.java))
