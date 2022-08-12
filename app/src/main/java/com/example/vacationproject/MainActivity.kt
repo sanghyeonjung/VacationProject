389d6f74b37e96cf1349d08588a31f555ea4f7a7
nrrr913 <narin2777@naver.com>
2022-08-12 11:46:18 +0900

    Update

--- app/src/main/java/com/example/vacationproject/MainActivity.kt
+++ app/src/main/java/com/example/vacationproject/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nextBtn = findViewById<TextView>(R.id.layoutFirstStudyTextButtonMain)
+        val nextBtn = findViewById<ConstraintLayout>(R.id.layoutFirstStudyMain)
         val quizBtn = findViewById<ConstraintLayout>(R.id.layoutQuizMain)
         val dockdo = findViewById<ImageButton>(R.id.layoutDokdo)
         val sky = findViewById<ImageButton>(R.id.skyBtn)
