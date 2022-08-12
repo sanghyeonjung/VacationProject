90f37ee7029ad0aa9511462b32df6ce0c5f7ee0b
jeong <roy1109@naver.com>
2022-08-12 12:29:50 +0900

    Feat : Create RecyclerViewAdapter.kt

--- app/src/main/java/com/example/vacationproject/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/vacationproject/RecyclerViewAdapter.kt
@@ -7,5 +7,22 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_list.view.*
 import java.util.*
 
-class RecyclerViewAdapter(private val items: ArrayList<DataScoreBoard>){
+class RecyclerViewAdapter(private val items: ArrayList<DataScoreBoard>):RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>()
+{
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            fun onBind(item : DataScoreBoard) {
+
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.onBind(items[position])
+        }
+
+        override fun getItemCount(): Int = items.size
 }
